a67012b6644e3c77bbb1ea88d6bc803aafcda634
Fervid-prog <Fervid.prog@gmail.com>
2026-06-17 14:09:17 -0400

    Feature : UI cell personalization

--- ExcelPapy/Objects/CellPersonalization.xaml.cs
+++ ExcelPapy/Objects/CellPersonalization.xaml.cs
@@ -13,21 +13,17 @@ public sealed partial class CellPersonalization : UserControl
         SelectedBorderText.Text = "Bord du Haut";
         UpdateSelectedIcon("Top");
 
-        SelectedBackgroundText.Text = "Fond du Haut";
-        UpdateBackgroundIcon("Top");
+        SelectedBackgroundText.Text = "Couleur";
+        UpdateBackgroundIcon("Blanc");
     }
 
     private void OnBorderPickerClick(object sender, RoutedEventArgs e)
     {
-        BorderPickerPopup.Width = BorderPickerButton.ActualWidth;
+        BorderPickerBorder.Width = BorderPickerButton.ActualWidth;
         BorderPickerPopup.IsOpen = !BorderPickerPopup.IsOpen;
     }
 
-    private void OnBackgroundPickerClick(object sender, RoutedEventArgs e)
-    {
-        BackgroundPickerPopup.Width = BackgroundPickerButton.ActualWidth;
-        BackgroundPickerPopup.IsOpen = !BackgroundPickerPopup.IsOpen;
-    }
+    
 
     private void OnBorderSelected(object sender, RoutedEventArgs e)
     {
@@ -53,29 +49,7 @@ public sealed partial class CellPersonalization : UserControl
         }
     }
 
-    private void OnBackgroundSelected(object sender, RoutedEventArgs e)
-    {
-        if (sender is Button btn)
-        {
-            var tag = btn.Tag?.ToString();
-            SelectedBackgroundText.Text = tag switch
-            {
-                "Top" => "Bordure Supérieure",
-                "Left" => "Bordure Gauche",
-                "Right" => "Bordure Droite",
-                "Bottom" => "Bordure Inférieure",
-                "None" => "Aucun Bord",
-                "All" => "Toutes les Bordures",
-                "Outer" => "Bordures Extérieurs",
-                _ => "Bordure..."
-            };
-
-            //Mettre à jour l'icône du bouton principal
-            UpdateSelectedIcon(tag);
-
-            BackgroundPickerPopup.IsOpen = false;
-        }
-    }
+   
 
     private void UpdateSelectedIcon(string? tag)
     {
@@ -108,37 +82,48 @@ public sealed partial class CellPersonalization : UserControl
             SelectedBorderIcon.Children.Add(line);
         }
     }
-}
 
-private void UpdateBackgroundIcon(string? tag)
+    private void OnBackgroundPickerClick(object sender, RoutedEventArgs e)
     {
-        SelectedBackgroundIcon.Children.Clear();
+        BackgroundPickerBorder.Width = BackgroundPickerButton.ActualWidth;
+        BackgroundPickerPopup.IsOpen = !BackgroundPickerPopup.IsOpen;
+    }
 
-        var lines = tag switch
+    private void OnBackgroundSelected(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn)
         {
-            "Top" => new[] { (0, 24, 0, 0, false), (0, 24, 12, 12, true), (0, 24, 24, 24, true), (0, 0, 0, 24, true), (12, 12, 0, 24, true), (24, 24, 0, 24, true) },
-            "Left" => new[] { (0, 24, 0, 0, true), (0, 24, 12, 12, true), (0, 24, 24, 24, true), (0, 0, 0, 24, false), (12, 12, 0, 24, true), (24, 24, 0, 24, true) },
-            "Right" => new[] { (0, 24, 0, 0, true), (0, 24, 12, 12, true), (0, 24, 24, 24, true), (0, 0, 0, 24, true), (12, 12, 0, 24, true), (24, 24, 0, 24, false) },
-            "Bottom" => new[] { (0, 24, 0, 0, true), (0, 24, 12, 12, true), (0, 24, 24, 24, false), (0, 0, 0, 24, true), (12, 12, 0, 24, true), (24, 24, 0, 24, true) },
-            "All" => new[] { (0, 24, 0, 0, false), (0, 24, 12, 12, false), (0, 24, 24, 24, false), (0, 0, 0, 24, false), (12, 12, 0, 24, false), (24, 24, 0, 24, false) },
-            "Outer" => new[] { (0, 24, 0, 0, false), (0, 24, 12, 12, true), (0, 24, 24, 24, false), (0, 0, 0, 24, false), (12, 12, 0, 24, true), (24, 24, 0, 24, false) },
-            _ => new[] { (0, 24, 0, 0, true), (0, 24, 12, 12, true), (0, 24, 24, 24, true), (0, 0, 0, 24, true), (12, 12, 0, 24, true), (24, 24, 0, 24, true) }
-        };
-
-        foreach (var (x1, x2, y1, y2, dashed) in lines)
-        {
-            var line = new Line
+            var colorname = btn.Tag?.ToString();
+            SelectedBackgroundText.Text = colorname switch
             {
-                X1 = x1,
-                X2 = x2,
-                Y1 = y1,
-                Y2 = y2,
-                Stroke = new SolidColorBrush(Microsoft.UI.Colors.Black),
-                StrokeThickness = 2
+                "White" => "Fond Blanc",
+                "Green" => "Fond Vert",
+                "Purple" => "Fond Violet",
+                "Pink" => "Fond Rose",
+                "Blue" => "Fond Bleu",
+                "Red" => "Fond Rouge",
+                "Yellow" => "Fond Jaune",
+                _ => "Fond de Couleur..."
             };
-            if (dashed)
-                line.StrokeDashArray = new DoubleCollection { 2, 3 };
-            SelectedBackgroundIcon.Children.Add(line);
+
+            SelectedBackgroundIcon.Background = colorname switch
+            {
+                "White" => new SolidColorBrush(Microsoft.UI.Colors.White),
+                "Red" => new SolidColorBrush(Microsoft.UI.Colors.Red),
+                "Blue" => new SolidColorBrush(Microsoft.UI.Colors.Blue),
+                "Yellow" => new SolidColorBrush(Microsoft.UI.Colors.Yellow),
+                "Green" => new SolidColorBrush(Microsoft.UI.Colors.Green),
+                "Purple" => new SolidColorBrush(Microsoft.UI.Colors.Purple),
+                "Pink" => new SolidColorBrush(Microsoft.UI.Colors.Pink),
+                _ => new SolidColorBrush(Microsoft.UI.Colors.White)
+            };
+
+            BackgroundPickerPopup.IsOpen = false;
         }
+    }
+
+    private void UpdateBackgroundIcon(string? colorname)
+    {
+           
     }
 }
